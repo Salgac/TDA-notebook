9178ff0efd4a3b76322a3e4e02025c6527bf5fac
Salgac <domco.salgovic@gmail.com>
2023-10-10 17:52:54 +0200

    Format

--- lib/TDataFrame.py
+++ lib/TDataFrame.py
@@ -1,30 +1,30 @@
 import pandas as pd
 import numpy as np
 
+
 class TDataFrame:
-    def __init__(self, filename ,df):
+    def __init__(self, filename, df):
         self.df = df
         self.date = filename.split("_")[2]
         self.vehicle = filename.split("_")[1]
-        
+
         # line data
-        self.line = str((df.loc[:,'IS_Cislo_sluzby']).head(1).iloc[0])
+        self.line = str((df.loc[:, "IS_Cislo_sluzby"]).head(1).iloc[0])
         try:
-            self.line_num = int(self.line[0]) 
+            self.line_num = int(self.line[0])
             self.line_order = int(self.line[1])
             self.line_mode = int(self.line[3])
         except ValueError:
-            self.line_num = 0 
+            self.line_num = 0
             self.line_order = 0
             self.line_mode = 0
-    
+
         # geo data
-        self.ll = df.get(['IS_Latitude', 'IS_Longitude', 'NudzBr_1']).dropna()
-        self.emb = list(zip(
-                           self.ll.loc[df['NudzBr_1'] == 1]["IS_Latitude"], 
-                           self.ll.loc[df['NudzBr_1'] == 1]["IS_Longitude"]
-                           ))
-        #self.bell = list(self.ll.loc[df['NudzBr_1'] == 1].drop(columns=['NudzBr_1']).to_records(index=False))
-         
-        
-    
+        self.ll = df.get(["IS_Latitude", "IS_Longitude", "NudzBr_1"]).dropna()
+        self.emb = list(
+            zip(
+                self.ll.loc[df["NudzBr_1"] == 1]["IS_Latitude"],
+                self.ll.loc[df["NudzBr_1"] == 1]["IS_Longitude"],
+            )
+        )
+        # self.bell = list(self.ll.loc[df['NudzBr_1'] == 1].drop(columns=['NudzBr_1']).to_records(index=False))
--- lib/TFilter.py
+++ lib/TFilter.py
@@ -1,19 +1,20 @@
 import pandas as pd
 
+
 def filter_tdfs(dfs, line):
     # filter dfs by every setting
-    filtered = dfs if line == '*' else [df for df in dfs if df.line_num == line]
-    
+    filtered = dfs if line == "*" else [df for df in dfs if df.line_num == line]
+
     return filtered
-    
+
+
 def extract_ll(data):
     # create an array of points
     lat_lon = []
-    
+
     for tdf in data:
         for c in tdf.emb:
-            #lat_lon.append((c[0], c[1], f"Line {tdf.line}"))
+            # lat_lon.append((c[0], c[1], f"Line {tdf.line}"))
             lat_lon.append(f"POINT ({c[0]} {c[1]})")
-    
+
     return lat_lon
-    
