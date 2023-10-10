5136d3a0322682647a70e4a87d078c395949c163
Salgac <domco.salgovic@gmail.com>
2023-10-10 18:08:43 +0200

    Implement date filter

--- lib/TFilter.py
+++ lib/TFilter.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 
-def filter_tdfs(dfs, line):
+def filter_tdfs(dfs, line, date):
     # filter dfs by every setting
     filtered = dfs if line == "*" else [df for df in dfs if df.line_num == line]
+    filtered = filtered if date == "*" else [df for df in filtered if df.date == date]
 
     return filtered
 
