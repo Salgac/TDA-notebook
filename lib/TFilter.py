1ff41dd298eb476ae222987c4933f51b96aca10b
Salgac <domco.salgovic@gmail.com>
2023-10-12 14:39:18 +0200

    Implement date range filter

--- lib/TFilter.py
+++ lib/TFilter.py
@@ -1,9 +1,15 @@
 import pandas as pd
+import time
 
 
 def filter_tdfs(dfs, line, date):
     # filter dfs by every setting
     filtered = dfs if line == "*" else [df for df in dfs if df.line_num == line]
-    filtered = filtered if date == "*" else [df for df in filtered if df.date == date]
+
+    # dates
+    d1, d2 = map(lambda date_str: time.strptime(date_str, "%d.%m.%Y"), date)
+    filtered = list(
+        filter(lambda df: d1 <= time.strptime(df.date, "%d.%m.%Y") <= d2, filtered)
+    )
 
     return filtered
